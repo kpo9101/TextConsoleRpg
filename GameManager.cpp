d36168a1900e7d1237c381d03704754597a315a7
Trued <qkek102090@gmail.com>
2026-03-26 19:48:50 +0900

    [이시준] 플레이어 기능구현 파일

---
+++ GameManager.cpp
@@ -0,0 +1,9 @@
+// GameManager.cpp
+
+#include "GameManager.h"
+#include "Monster.h"
+#include "PlayerCharacter.h"
+#include <iostream>
+#include <cstdlib>
+
+
---
+++ GameManager.h
@@ -0,0 +1,6 @@
+//GameManager.h
+
+#pragma once 
+#include "PlayerCharacter.h"
+#include "Monster.h"
+
