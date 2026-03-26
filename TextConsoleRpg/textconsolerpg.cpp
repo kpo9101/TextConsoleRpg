fb7a7c9216cd5857fe398f670f280c81b62a007a
kpo9101 <kpo9101@gmail.com>
2026-03-27 02:10:59 +0900

    [test]

--- TextConsoleRpg/textconsolerpg.cpp
+++ TextConsoleRpg/textconsolerpg.cpp
@@ -13,31 +13,39 @@ int main(void)
 	std::cout << std::endl << "용사님, 당신의 이름을 입력하십시오." << std::endl;
 	std::getline(std::cin, Name);
 	std::cout << "당신의 이름은 \"" << Name << "\" 이군요" << std::endl;
-	std::cout << std::endl << "당신은 고민합니다." << std::endl;
-	std::cout << "[1]나는 이 상황을 변화시킬 힘이 없다 용사를 포기한다." << std::endl;
-	std::cout << "[2]나는 그림자 마왕을 쓰러뜨리고, 왕국의 보물을 되찾을 것이다." << std::endl;
-	std::cin >> select;
-	if (std::cin.fail())
-	{
-		std::cin.clear();
-		std::cin.ignore(100, '\n');
-	}
-	if (select == 1)
-	{
-		std::cout << "당신은 왕국과 용사가 되기를 포기하고 스파르타 왕국을 떠났습니다..." << std::endl;
-		return 0;
-	}
-	else if (select == 2)
-	{
-		std::cout << "당신은 용사가 되어 왕국의 보물을 되찾고 결계를 복구하여 모두를 지킬 것을 선택했습니다." << std::endl;
-		PlayerCharacter* Player = new PlayerCharacter(Name);
 
-		delete Player;
-		Player = nullptr;
-	}
-	else
+	while (true)
 	{
-		std::cout << "잘못된 입력입니다 다시 입력하십시오.";
+		std::cout << std::endl << "당신은 고민합니다." << std::endl;
+		std::cout << "[1]나는 이 상황을 변화시킬 힘이 없다 용사를 포기한다." << std::endl;
+		std::cout << "[2]나는 그림자 마왕을 쓰러뜨리고, 왕국의 보물을 되찾을 것이다." << std::endl;
+		std::cin >> select;
+		if (std::cin.fail())
+		{
+			std::cin.clear();
+			std::cin.ignore(100, '\n');
+		}
+		if (select == 1)
+		{
+			std::cout << "당신은 왕국과 용사가 되기를 포기하고 스파르타 왕국을 떠났습니다..." << std::endl;
+			return 0;
+		}
+		else if (select == 2)
+		{
+			std::cout << "당신은 용사가 되어 왕국의 보물을 되찾고 결계를 복구하여 모두를 지킬 것을 선택했습니다." << std::endl;
+			PlayerCharacter* Player = new PlayerCharacter(Name);
+
+			delete Player;
+			Player = nullptr;
+		}
+		else
+		{
+			std::cout << "잘못된 입력입니다! '1'또는 '2'를 입력하십시오.";
+			continue;
+		}
+
+		
 	}
+	
 	return 0;
 }
